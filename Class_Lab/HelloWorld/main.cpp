8b1a0220fcb8d28144f9c53637b3591289b49c2c
lh1705617 <lherrera3@student.rcc.edu>
2015-01-06 20:03:04 -0800

    Signed-off-by: lh1705617 <lherrera3@student.rcc.edu>

---
+++ Class_Lab/HelloWorld/main.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   main.cpp
+ * Author: Laura Herrera
+ * Created on January 6, 2015, 6:05 PM
+ * Purpose: Our first program
+ */
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //output Hello World
+    cout<<"Hello World"<<endl;
+         
+    return 0;
+}
+
