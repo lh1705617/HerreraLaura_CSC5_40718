75476a8472b9a046fed9962e3b867c0b9c3cfa5f
lh1705617 <lherrera3@student.rcc.edu>
2015-01-11 19:51:34 -0800

    Signed-off-by: lh1705617 <lherrera3@student.rcc.edu>

--- Homework/Gaddis_7thEd_Chap2_Problem11/main.cpp
+++ Homework/Gaddis_7thEd_Chap2_Problem11/main.cpp
@@ -18,15 +18,15 @@ int main(int argc, char** argv) {
     //Declare and initialize variables
     float gasTank=20;//Max gallons in our gas tank
     float inTown=21.5;//In town miles per gallon
-    float onHway=26.8;//Highway miles per gallon
+    float onHwy=26.8;//Highway miles per gallon
     //calculate the total miles in and out of town
     float disTown=gasTank*inTown;
-    float disHway=gasTank*onHway;
+    float disHwy=gasTank*onHwy;
     //Output the results
-    cout<<"Distance in town on a tank of gas      =";
+    cout<<"Distance in town on a tank of gas      = ";
     cout<<disTown<<"(miles)"<<endl;
-    cout<<"Distance on highway with a tank of gas =";
-    cout<<disHway<<"(miles)"<<endl;
+    cout<<"Distance on highway with a tank of gas = ";
+    cout<<disHwy<<"(miles)"<<endl;
      //Exit stage right!
     return 0;
 }
