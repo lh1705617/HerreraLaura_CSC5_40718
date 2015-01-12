b35dfd937e73c207c7a7400abcdffcccd71860d2
lh1705617 <lherrera3@student.rcc.edu>
2015-01-11 17:15:44 -0800

    Signed-off-by: lh1705617 <lherrera3@student.rcc.edu>

---
+++ Homework/Gaddis_7thEd_Chap2_Problem1/main.cpp
@@ -0,0 +1,31 @@
+/* 
+ * File:   main.cpp
+ * Author: Laura Herrera    
+ * Purpose: Gaddis 7thEd Ch2 Problem 1
+ *
+ * Created on January 11, 2015, 2:18 AM
+ */
+//System Libraries
+#include <iostream>
+
+using namespace std;
+
+//Global Constants
+//Function Prototypes
+/*
+ //Execution begins here
+ */
+int main() 
+{
+ int num1 = 62;
+ int num2 = 99;
+ int total = num1+num2;
+ 
+ //Calculate num1 + num2
+ 
+ cout<<"62+99 = "<<total<<endl;
+ //Exit stage left! 
+    
+    return 0;
+}
+
